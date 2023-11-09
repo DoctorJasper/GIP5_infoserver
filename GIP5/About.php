66a10d50b8cebc396bf7ce00724d520df7de5ce9
KyanVDK <122273798+NRKyan345@users.noreply.github.com>
2023-11-09 15:29:49 +0100

    Add files via upload

---
+++ GIP5/About.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<?php require("header.php"); ?>
+<div class="container mt-5">
+  <div class="row justify-content-center">
+    <div class="col-sm-12">
+      <div class="card">
+        <div class="card-header">
+          Over ons
+        </div>
+        <div class="card-body">
+          <form action="process_account.php" method="post">
+            <div class="form-group">
+            <h2>Over ons</h2>
+              <p>Wij zijn Jasper T'Kindt en Kyan Vanderkelen, studenten in de richting van 6 informaticabeheer. Ons gezamenlijk GIP-project richt zich op de implementatie van een 'Info Server' voor leerlingen in informaticarichtingen. We kregen inzicht in moeilijkheden met het bewaren, updaten en verwijderen van leerlingengegevens. Ons doel is het creëren van een efficiënte info server met een gebruiksvriendelijke toegang.
+                De info server zal diverse functionaliteiten omvatten, zoals gefilmde en geschreven tutorials voor leerlingen die minder bekend zijn met computers of informatica. Het beheer voor admins zal sterk geoptimaliseerd worden met diverse knoppen voor snelle aanpassingen van leerlinggegevens. Deze opdracht is voor ons beiden van groot belang en komt perfect uit door onze sterke vriendschapsband en gedeelde passie voor dit onderwerp.
+                Belangrijke aspecten van ons project omvatten inlogmogelijkheden, account- en klasbeheer, tutorials en het beheer van logbestanden voor foutmonitoring. Dit project streeft naar een geïntegreerde webapplicatie die via een webbrowser toegankelijk is en verbonden is met het bestaande Dashboard.
+                Ons doel is het creëren van een efficiënt systeem dat de beheerders ondersteunt in het gebruik van gegevens. Hoewel we nog lessen en uitleg zullen ontvangen met betrekking tot het koppelen van databases met code, zijn we vastberaden om aan de verwachtingen te voldoen.</p>
+                
+                <br>
+
+                <h2>Onderzoeksvraag</h2>
+                <p>De kern van ons project is gericht op het verbeteren van tijdsbeheer en het vereenvoudigen van taken voor administrators die de info server beheren. Leerlingen en leerkrachten krijgen de mogelijkheid om zelfstandig hun accounts te beheren, waardoor de werklast van de administrators verlicht wordt. De integratie met Smartschool maakt een gemakkelijke toegang tot het systeem mogelijk voor alle gebruikers. Belangrijke functionaliteiten omvatten accountcreatie, -wijziging, -beheer, toegankelijke tutorials en foutdetectie via logbestanden.</p>
+                
+                <br>
+
+                <h2>Ontwikkelingsproces</h2>
+                <p>Ons project wordt ontwikkeld met behulp van CSS, HTML, Linux, JavaScript, PHP en MD Bootstrap 5. PHP zal fungeren als de brug tussen de database en de code. De overige genoemde talen worden ingezet voor de lay-out en vormgeving van de website.
+                De database zal gegevens van beheerders en leerlingen bevatten. Via SQL en PHP zullen we deze gegevens vanuit de code kunnen oproepen. Beheerders hebben de mogelijkheid om gegevens aan te passen, updaten of verwijderen. Leerlingen kunnen hun wachtwoord wijzigen en hun gegevens updaten.</p>
+                
+                <br>
+
+                <h2>Opdrachtgever</h2>
+                <p>Onze opdrachtgever is Bart Kindt, leerkracht wiskunde en informatica in GO! atheneum Oudenaarde, te bereiken via Bart.Kindt@go-ao.be.</p>
+
+                <br>
+
+                <h2>Functionele Eisen</h2>
+                <p>Het project omvat functionaliteiten zoals inlogmogelijkheden, accountbeheer, klasbeheer, tutorials en een directe link naar de persoonlijke homepage van de gebruikers.</p>
+
+                <br>
+
+                <h2>Conclusie</h2>
+                <p>Het "Info Server" project beoogt een geïntegreerd platform te zijn dat administratieve taken rondom account- en klasbeheer vereenvoudigt. Ons gebruik van diverse technologieën zoals CSS, HTML, Linux, JavaScript, PHP en MD Bootstrap 5 zal resulteren in een goed gestructureerde en visueel aantrekkelijke webapplicatie.
+                Wij hebben hoge verwachtingen voor dit project en streven ernaar te voldoen aan de eisen van onze opdrachtgever. Persoonlijk kijken we uit naar het opdoen van waardevolle inzichten en competenties en het versterken van onze samenwerking. Dit project zal naar verwachting een positieve impact hebben op de school en een verbetering betekenen voor beheerders en gebruikers.</p>
+
+                <br>
+
+                <a class="nav-link" href="Index.php">Terug</a>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+</body>
+</html>
---
+++ GIP5/userNew.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<?php
+require("startphp.php");
+
+if (!isset($_SESSION['username'])) {
+    //user is reeds aangemeld
+    header("Location: login.php");
+    exit;
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    require("pdo.php");
+    $username = trim($_POST["username"]);
+    $naam = trim($_POST["naam"]);
+    $voornaam = trim($_POST["voornaam"]);
+    $email = trim($_POST["email"]);
+    $admin = isset($_POST["admin"]) ? 1 : 0;
+    $password = password_hash(trim($_POST["password"]), PASSWORD_DEFAULT);
+
+    //create GUID
+    $GUID = sprintf('%04X%04X-%04X-%04X-%04X-%04X%04X%04X', mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(16384, 20479), mt_rand(32768, 49151), mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(0, 65535));
+
+    //Update query template
+    $query = "INSERT INTO `tblGebruiker`(`GUID`,`userName`,`naam`,`voornaam`,`email`,`userPassword`,`admin`)
+              VALUES (:ID, :userName, :naam, :voornaam, :email, :userPassword, :adm)";
+
+    //Values array for PDO
+    $values = [":ID" => $GUID, ":userName" => $username, ":naam" => $naam, ":voornaam" => $voornaam,
+               ":email" => $email, ":userPassword" => $password, ":adm" => $admin];
+
+    //Execute the query
+    try {
+        $res = $pdo->prepare($query);
+        $res->execute($values);
+        header("Location: adminpage.php");
+        exit;
+    } catch (PDOException $e) 
+    {
+        echo "Guery error.<br>".$e;
+        die();
+    }
+}
+require("header.php");
+?>
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-sm-6">
+            <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+                    <div class="mb-3">
+                        <label for="Username" class="form-label">Gebruikersnaam</label>
+                        <input type="text" class="form-control" id="Username" name="username" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="Naam" class="form-label">Naam</label>
+                        <input type="text" class="form-control" id="Naam" name="naam" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="Voornaam" class="form-label">Voornaam</label>
+                        <input type="text" class="form-control" id="Voornaam" name="voornaam" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="Email" class="form-label">Email</label>
+                        <input type="email" class="form-control" id="Email" name="email" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="Password" class="form-label">Wachtwoord</label>
+                        <input type="password" class="form-control" id="Password" name="password" required>
+                    </div>
+                    <div class="form-check form-switch">
+                        <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
+                        <label class="form-check-label" for="flexSwitchCheckDefault">Admin</label>
+                    </div>
+                    <br>
+                    <button type="submit" class="btn btn-success">Gebruiker aanmaken</button>
+                </form>
+            </div>
+            <div class="col-sm-6">
+            </div>
+        </div>
+    </div>
+</body>
+</html>
