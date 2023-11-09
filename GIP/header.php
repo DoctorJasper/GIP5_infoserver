6b2f5d67a99625cb360e0890fe3ebc0cfa18f700
KyanVDK <122273798+NRKyan345@users.noreply.github.com>
2023-11-09 08:58:36 +0100

    Add files via upload

---
+++ GIP/header.php
@@ -0,0 +1,41 @@
+<head>
+  <title>CNU</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+  <style>
+    body {
+      background-color: #f2e6ff;
+    }
+    .btn-primary {
+      background-color: #1a3b9e; 
+      border-color: #1a3b9e;
+    }
+    .btn-primary:hover {
+      background-color: #993399; 
+      border-color: #993399;
+    }
+    .navbar {
+      background: linear-gradient(to right, #000000, #1a3b9e); 
+    }
+    .navbar-dark .navbar-nav .nav-link {
+      color: #ffffff;
+    }
+    .navbar-dark .navbar-toggler-icon {
+      filter: invert(1) brightness(100);
+    }
+    .card {
+      background-color: #ffffff; 
+      color: #000000;
+    }
+    .card-header {
+      background: linear-gradient(to right, #000000, #1a3b9e);
+      color: #ffffff;
+    }
+    .card-body {
+      color: #000000;
+    }
+  </style>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+  
+</head>
