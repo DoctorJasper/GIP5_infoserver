2f3d67776e6e5620832709e60b345fd6b8a7e99e
De Jasper T'Kindt <122273835+DoctorJasper@users.noreply.github.com>
2023-11-21 08:56:29 +0100

    Add files via upload

--- GIP5/adminpage.php
+++ GIP5/adminpage.php
@@ -8,43 +8,73 @@ if (!isset($_SESSION["username"]) && $_SESSION["admin"] != 1) {
 } elseif (isset($_SESSION["username"]) && $_SESSION["admin"] != 1) {
     header("Location: About.php");
     exit();
-}
+}   
+require("HeaderAdminpage.php");
 
-require("header.php");
 ?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Page</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+       
+    </style>
+</head>
+<body>
 
-<div class="container mt-5">
-  <div class="row justify-content-center">
-    <div class="col-sm-6">
-      <div class="card">
-        <div class="card-header">
-          Welcome
-        </div>
-        <div class="card-body">
-          <div class="row">
-            <div class="col-md-4">
-              <img src="./Images/Pfp.jpg" class="img-fluid" alt="Sample Image">
-            </div>
-            <div class="col-md-8">
-              <h1>Welkom ADMIN</h1>
-              <p>Op deze pagina kunt u de klassen en leerlingen beheren</p>
-            </div>
-          </div>
-        </div>
-      </div>
-      <br>
-      <div class="dropdown">
-        <button class="btn btn-primary dropdown-toggle" type="button" id="Klassen" data-bs-toggle="dropdown" aria-expanded="false">
-          Selecteer
-        </button>
-        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-          <li><a class="dropdown-item" href="#">6 INFO</a></li>
-          <li><a class="dropdown-item" href="#">5 APDA</a></li>
-          <li><a class="dropdown-item" href="#">smt smt</a></li>
+<nav class="navbar navbar-expand-lg navbar-dark">
+    <a class="navbar-brand" href="#">Infoserver</a>
+</nav>
+
+<div class="container-fluid">
+    <div class="row">
+        <nav class="col-md-2 d-none d-md-block sidebar">
+            <div class="sidebar-sticky">
+            <ul class="navbar-nav ms-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="About.php">Over ons &nbsp; <i class="bi bi-info-circle"></i></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="https://go-ao.smartschool.be/helpdesk#!tickets/list/4ca0ce7d-eeb0-4842-802e-8c5701705bcf" target="_blank">Contact &nbsp; <i class="bi bi-telephone"></i> </a>
+          </li>
+          <?php if (!isset($_SESSION["username"])): ?>
+              <li class="nav-item">
+                  <a class="nav-link" href="login.php">login</a>
+              </li>
+          <?php else: ?>
+              <li class="nav-item">
+                  <a class="nav-link" href="logout.php">logout &nbsp; <i class="bi bi-box-arrow-right"></i></a>
+              </li>
+          <?php endif; ?>
+          <li class="nav-item">
+                  <a class="nav-link" href="userOverview.php">Overvieuw &nbsp;<i class="bi bi-database"></i></a>
+              </li>
+              <li class="nav-item">
+                  <a class="nav-link" href="usernew.php">Nieuwe User &nbsp; <i class="bi bi-person-plus"></i></a>
+              </li>
         </ul>
-      </div>
+            </div>
+        </nav>
+
+        <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
+          <br>
+            <div class="card">
+                <div class="card-header">
+                    <h1>Welkom ADMIN</h1>
+                </div>
+                <div class="col-md-4 mx-auto text-center">
+                    <img src="./Images/Pfp.jpg" class="img-fluid" alt="Sample Image">
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title text-center">Dashboard Overview</h5>
+                    <p class="card-text text-center">Op deze pagina kan u de leerlingen beheren</p>
+                </div>
+            </div>
+        </main>
     </div>
-  </div>
 </div>
+
 </body>
 </html>
