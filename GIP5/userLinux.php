de2a129842869560ceddf3780a2fcb5bbc956530
NRKyan345 <kyan.vanderkelen3@gmail.com>
2024-05-27 18:12:18 +0200

    del linuxUser

--- GIP5/userLinux.php
+++ GIP5/userLinux.php
@@ -110,21 +110,21 @@ function handleAction($actie, $leerlingenIntNr) {
         foreach ($namenLeerlingen as $naamLeerling) {
             $username = $naamLeerling["username"];
 
-            $query = "SELECT `commando` FROM `tblCommandos` WHERE `idPlatform` = 1 AND `type` = 'verwijderen'";
+            $query = "SELECT `commandos` FROM `tblCommandos` WHERE `idPlatform` = 1 AND `type` = 'verwijderen'";
         
             try {
                 $res = $pdo->prepare($query);
                 $res->execute();
-                $commando = $res->fetch(PDO::FETCH_ASSOC)['commando'];
+                $commando = $res->fetch(PDO::FETCH_ASSOC)['commandos'];
                 $commando = str_replace("gebruikersnaam", $username, $commando);
 
                 file_put_contents("log.txt", date("Y-m-d H:i:s") . " || Command to execute: " . $commando . PHP_EOL, FILE_APPEND);
                 
                 exec($commando);
+                $toast->set("fa-exclamation-triangle", "Note", "", "Linux user '$username' verwijderd", "success");
             } catch (PDOException $e) {
                 file_put_contents("log.txt", date("Y-m-d H:i:s") . " || Database query error: " . $e->getMessage() . PHP_EOL, FILE_APPEND);
-            } catch (Exception $e) {
-                file_put_contents("log.txt", date("Y-m-d H:i:s") . " || Command execution error: " . $e->getMessage() . PHP_EOL, FILE_APPEND);
+                $toast->set("fa-exclamation-triangle", "Error", "", "Gefaald om linux user '$username' te verwijderen", "danger");
             }
 
             // Insert into tblAccounts
