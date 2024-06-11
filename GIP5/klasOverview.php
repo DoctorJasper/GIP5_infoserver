c6e9bff11b20951224b951967b3b7d7c82fc9694
NRKyan345 <kyan.vanderkelen3@gmail.com>
2024-06-11 21:36:54 +0200

    userNew table update

--- GIP5/klasOverview.php
+++ GIP5/klasOverview.php
@@ -41,6 +41,14 @@
         margin-right: 75px; 
         margin-top: 40px;
     }
+    #scrollable-table {
+        height: 400px;
+        overflow-y: auto;
+        margin-bottom: 20px;
+    }
+    body {
+        overflow: hidden;
+    }
 </style>
 <?php require('../navbar.php') ;?>
 <br><br>
@@ -59,26 +67,28 @@
                         <div class="card-header bg-warning bg-gradient">
                             <h1 class="text-white center">Mijn klassen<h1>
                         </div>
-                        <table class="table table-hover table-striped">
-                            <tr>
-                                <th>klas</th>
-                                <th>acties</th>
-                            </tr>
-                            <?php if ($res->rowCount() != 0) : ?>
-                                <?php while($row = $res->fetch(PDO::FETCH_ASSOC)) : ?>
-                                    <tr>
-                                        <td><a class="text-black" href="userNew.php?klas=<?php echo $row["klas"]; ?>"><?php echo$row["klas"]; ?></a></td>
-                                        <td> 
-                                            <!-- Verwijder-knop voor klassen -->
-                                            <a href="klasDelete.php?klas=<?php echo $row["klas"] ;?>"><i id="Delete" class="bi bi-trash text-danger fs-5" data-bs-toggle="modal" data-bs-target="#DeleteKlas" data-bs-toggle="tooltip" data-bs-placement="top" title="Verwijder klas"></i></a>
-                                        </td>
-                                    </tr>
-                                <?php endwhile; ?>
-                            <?php else : ?>
-                                <!-- Melding voor geen gevonden gegevens -->
-                                <tr><td colspan="6">Geen gegevens gevonden</td></tr>
-                            <?php endif; ?>
-                        </table>
+                        <div id="scrollable-table">
+                            <table class="table table-hover table-striped">
+                                <tr>
+                                    <th>klas</th>
+                                    <th>acties</th>
+                                </tr>
+                                <?php if ($res->rowCount() != 0) : ?>
+                                    <?php while($row = $res->fetch(PDO::FETCH_ASSOC)) : ?>
+                                        <tr>
+                                            <td><a class="text-black" href="userNew.php?klas=<?php echo $row["klas"]; ?>"><?php echo$row["klas"]; ?></a></td>
+                                            <td> 
+                                                <!-- Verwijder-knop voor klassen -->
+                                                <a href="klasDelete.php?klas=<?php echo $row["klas"] ;?>"><i id="Delete" class="bi bi-trash text-danger fs-5" data-bs-toggle="modal" data-bs-target="#DeleteKlas" data-bs-toggle="tooltip" data-bs-placement="top" title="Verwijder klas"></i></a>
+                                            </td>
+                                        </tr>
+                                    <?php endwhile; ?>
+                                <?php else : ?>
+                                    <!-- Melding voor geen gevonden gegevens -->
+                                    <tr><td colspan="6">Geen gegevens gevonden</td></tr>
+                                <?php endif; ?>
+                            </table>
+                        </div>
                     </div>
                 </div>
                 <!-- EINDE OVERZICHT KLASSEN -->                
