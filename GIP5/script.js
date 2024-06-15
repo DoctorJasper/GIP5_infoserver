10ab75d235fcff2109fb1e77a1b24031a4bbdaad
NRKyan345 <kyan.vanderkelen3@gmail.com>
2024-06-15 17:33:28 +0200

    tutorial example

--- GIP5/script.js
+++ GIP5/script.js
@@ -12,26 +12,3 @@ document.addEventListener('DOMContentLoaded', function() {
         videoList.appendChild(videoList.firstElementChild);
     });
 });
-
-function openModal(videoElement) {
-    const modal = document.getElementById('videoModal');
-    const modalVideo = document.getElementById('modalVideo');
-
-    modalVideo.src = videoElement.src;
-    modal.style.display = 'block';
-}
-
-function closeModal() {
-    const modal = document.getElementById('videoModal');
-    const modalVideo = document.getElementById('modalVideo');
-
-    modal.style.display = 'none';
-    modalVideo.src = ''; // Stop the video when the modal is closed
-}
-
-window.onclick = function(event) {
-    const modal = document.getElementById('videoModal');
-    if (event.target == modal) {
-        closeModal();
-    }
-}
