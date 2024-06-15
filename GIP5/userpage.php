0893e5e0ed209b3f5e1e16064e176de52c205612
NRKyan345 <kyan.vanderkelen3@gmail.com>
2024-06-15 17:38:53 +0200

    tutorial example

--- GIP5/userpage.php
+++ GIP5/userpage.php
@@ -115,6 +115,7 @@
             <div class="col-sm-6">
                 <div class="video-container">
                     <ul class="video-list">
+                        <li></li>
                         <li class="video-item">
                             <video src="tutorials/Eminem - Lose Yourself [HD].mp4" controls></video>
                             <p>eminem</p>
